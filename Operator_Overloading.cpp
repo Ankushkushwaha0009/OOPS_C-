a32e24eff2c1f97630b6d6d2dacee60a05e3900b
Ankush kushwaha <ankushkushwaha2829@gmail.com>
2026-08-26 03:43:27 +0530

    operator overloading completed

--- Operator_Overloading.cpp
+++ Operator_Overloading.cpp
@@ -1,38 +1,89 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-class Complex {
-    public:
+// class Complex {
+//     public:
 
-        int real;
-        int imaginary;
-        
-        Complex(int r, int i) {
-            real = r;
-            imaginary = i;
-        }
+//         int real;
+//         int imaginary;
 
-        Complex operator+(const Complex& other) {
-            Complex result(
-                real + other.real,
-                imaginary + other.imaginary
-            );
-            return result;
-        }
-};
+//         Complex(int r, int i) {
+//             real = r;
+//             imaginary = i;
+//         }
 
-int main() {
+//         Complex operator+(const Complex& other) {
+//             Complex result(
+//                 real + other.real,
+//                 imaginary + other.imaginary
+//             );
+//             return result;
+//         }
+// };
 
-    Complex c1(3, 4);
-    Complex c2(5, 6);
-    
-    // c1.operator+(c2) 
-    // this -> c1 
-    // other -> c2 
+// int main() {
 
-    Complex c3 = c1 + c2; 
-    cout << c3.real << " + "
-         << c3.imaginary << "i" << endl;
+//     Complex c1(3, 4);
+//     Complex c2(5, 6);
 
-    return 0;
+//     // c1.operator+(c2)
+//     // this -> c1
+//     // other -> c2
+
+//     Complex c3 = c1 + c2;
+//     cout << c3.real << " + "
+//          << c3.imaginary << "i" << endl;
+
+//     return 0;
+// }
+
+// -------- using (=) Operator --------------
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class Student
+// {
+// public:
+//     int marks;
+//     Student(int marks)
+//     {
+//         this->marks = marks;
+//     }
+
+//     bool operator==(const Student &other)
+//     {
+//         return other.marks == marks;
+//     }
+// };
+
+// int main()
+// {
+//     Student s1(1000);
+//     Student s2(1000);
+//     if(s1 == s2) {
+//         cout << "Both are same ..." << endl ; 
+//     }
+//     return 0;
+// }
+
+#include<bits/stdc++.h>
+using namespace std  ; 
+class Number  {
+    public : 
+       int value ; 
+       Number(int value) {
+        this -> value =  value ; 
+       }
+       Number operator+(const Number &other) {
+           Number result(other.value + value) ; 
+           return result ; 
+       }
+} ; 
+int main()  {
+    Number n1(10) ; 
+    Number n2(20) ; 
+    Number n3 = n1 + n2 ; 
+    cout << n3.value << endl ; 
+    return  0 ;  
 }
