b870b4f4323851352346d4c387798133b9be97f7
Ankush kushwaha <ankushkushwaha2829@gmail.com>
2026-08-29 02:49:39 +0530

    added one another example

--- Template.cpp
+++ Template.cpp
@@ -10,19 +10,63 @@
 //     return  0 ; 
 // }
 
+// #include<bits/stdc++.h>
+// using namespace std ;
+
+// //function template..... 
+
+// template < typename T , typename K> 
+
+// T add(T a  , K b)  {
+//     return a + b  ; 
+// }
+
+// int main()  {
+//     cout << add(10, 12.0) << endl ; 
+//     cout << add(10.1  , 1.2) << endl  ; 
+//     return  0 ; 
+// }
+
+//--------------------------Exercise ------------------------------
+
+
+// #include <iostream>
+// using namespace std;
+
+// template <typename T>
+
+// T maximum(T a, T b) {
+//     if(a > b) return a ; 
+//     return b  ;     
+// }
+
+// int main() {
+//     cout << maximum(10, 20) << endl;
+//     cout << maximum(5.5, 2.2) << endl;
+//     return 0;
+// }
+
+// class template .....
+
 #include<bits/stdc++.h>
-using namespace std ;
-
-//function template..... 
-
-template < typename T , typename K> 
-
-T add(T a  , K b)  {
-    return a + b  ; 
-}
-
-int main()  {
-    cout << add(10, 12.0) << endl ; 
-    cout << add(10.1  , 1.2) << endl  ; 
-    return  0 ; 
+using namespace std ; 
+template < typename T > 
+class Box  {
+    public :  
+       T value ;
+       Box(T value)  {
+         this -> value = value ; 
+       }
+       void show() {
+        cout << value << endl;
+    }
+} ; 
+int main() {
+    Box < int > b1(100) ; 
+    Box < float > b2(100.111) ; 
+    Box <string> b3("ankush kushwaha") ; 
+    b1.show() ; 
+    b2.show() ; 
+    b3.show() ; 
+    return 0  ; 
 }
