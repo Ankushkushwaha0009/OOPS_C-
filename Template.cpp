a6fa0406219cf0b3f031e8c77f5cfecd20f01552
Ankush kushwaha <ankushkushwaha2829@gmail.com>
2026-08-29 02:40:13 +0530

    added another example of function teamplate

--- Template.cpp
+++ Template.cpp
@@ -1,11 +1,28 @@
+// #include<bits/stdc++.h>
+// using namespace std ; 
+// template < typename T > 
+// T add(T a  , T b)  {
+//     return a + b  ; 
+// }
+// int main()  {
+//     cout << add(10 , 12) << endl ; 
+//     cout << add(10.1  , 1.2) << endl  ; 
+//     return  0 ; 
+// }
+
 #include<bits/stdc++.h>
-using namespace std ; 
-template < typename T > 
-T add(T a  , T b)  {
+using namespace std ;
+
+//function template..... 
+
+template < typename T , typename K> 
+
+T add(T a  , K b)  {
     return a + b  ; 
 }
+
 int main()  {
-    cout << add(10 , 12) << endl ; 
+    cout << add(10, 12.0) << endl ; 
     cout << add(10.1  , 1.2) << endl  ; 
     return  0 ; 
 }
