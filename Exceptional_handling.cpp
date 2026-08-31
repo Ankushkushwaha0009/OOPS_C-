6bf777698d640f53cd383d96ae893393347cd2c2
Ankush kushwaha <ankushkushwaha2829@gmail.com>
2026-09-01 03:45:36 +0530

    practice is done for exceptional handling

--- Exceptional_handling.cpp
+++ Exceptional_handling.cpp
@@ -1,15 +1,51 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main() {
+//     try{
+//         int a = 10;
+//         int b = 0;
+//         if (b == 0)
+//             throw 1.0;
+//         cout << a / b;
+//     }
+//     catch (double e){
+//         cout << "Cannot divide ny zero" << endl;
+//     }
+//     return 0;
+// }
+
+//Example 2 ...............
+
+
+// #include <bits/stdc++.h>
+// using namespace std ; 
+// int divide( int a , int b) {
+//     if(b == 0)  throw "Division by zero" ; 
+//     return a / b ; 
+// }
+// int main() {
+//     try {
+//        cout << divide(10 , 0) ;  
+//     }catch(const char* message) {
+//        cout << message << endl ; 
+//     }
+//     return 0 ;
+// }
+
+//Example 3 (Practice)
+#include <iostream>
 using namespace std;
 int main() {
-    try{
-        int a = 10;
-        int b = 0;
-        if (b == 0)
-            throw 1.0;
-        cout << a / b;
+    int age;
+    cin >> age;
+    try {
+        if (age < 0) {
+           throw 1 ; 
+        }
+        cout << "Valid age" << endl;
     }
-    catch (double e){
-        cout << "Cannot divide ny zero" << endl;
+    catch(int e) {
+        cout << "Invalid age" << endl;
     }
     return 0;
 }
