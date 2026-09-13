0d3a54b1c3ef80ae66035228f6704c5904bffe7c
Ankush kushwaha <ankushkushwaha2829@gmail.com>
2026-09-14 02:31:34 +0530

    added some more code in 4 Pillars eg

--- 4_Pillars_Eg.cpp
+++ 4_Pillars_Eg.cpp
@@ -1,3 +1,100 @@
+// #include<bits/stdc++.h>
+// using namespace std ; 
+
+// class Payment {
+//     //Encapsulation ....
+//    private : 
+//       double amount ; 
+//     public : 
+//       Payment(double amount) {
+//          this  -> amount = amount ; 
+//       }
+//       double getAmount() {
+//         return amount  ;
+//       }
+//       //Abstraction ...
+//       virtual void pay() = 0 ; 
+// } ; 
+
+// class CreditCard : public Payment {
+//     public :  
+//     CreditCard(double amount) : Payment(amount) {}
+//     void pay() override{
+//         cout << "Paid : " << getAmount() << " using Credit Card\n";
+//     }
+// } ; 
+
+// class UPI  : public Payment {
+//     public : 
+//      UPI(double amount) : Payment(amount) {}   
+//       void pay() override {  cout << "Paid :" << getAmount() << " using UPI\n";
+//     }
+// } ; 
+
+// int main()  {
+//     Payment *p; 
+    
+//     UPI upi(500);
+//     CreditCard card(1000);
+
+//     p = &upi ; 
+//     p -> pay() ; 
+
+//     p = &card ;
+//     p -> pay() ; 
+
+//     return 0 ; 
+// }
+
+//Another way to write Main 
+//-------------------------------
+
+
+// #include<bits/stdc++.h>
+// using namespace std ; 
+
+// class Payment {
+//     //Encapsulation ....
+//    private : 
+//       double amount ; 
+//     public : 
+//       Payment(double amount) {
+//          this  -> amount = amount ; 
+//       }
+//       double getAmount() {
+//         return amount  ;
+//       }
+//       //Abstraction ...
+//       virtual void pay() = 0 ; 
+// } ; 
+
+// class CreditCard : public Payment {
+//     public :  
+//     CreditCard(double amount) : Payment(amount) {}
+//     void pay() override{
+//         cout << "Paid : " << getAmount() << " using Credit Card\n";
+//     }
+// } ; 
+
+// class UPI  : public Payment {
+//     public : 
+//      UPI(double amount) : Payment(amount) {}   
+//       void pay() override {  cout << "Paid :" << getAmount() << " using UPI\n";
+//     }
+// } ; 
+
+// int main()  {
+//     Payment *c  = new CreditCard(100) ; 
+//     Payment *u  = new UPI(200) ; 
+//     c -> pay() ; 
+//     u -> pay() ;
+//     return 0 ; 
+// }
+
+
+//Another way to write the main (using array)
+//---------------------------------------
+
 #include<bits/stdc++.h>
 using namespace std ; 
 
@@ -32,16 +129,11 @@ class UPI  : public Payment {
 } ; 
 
 int main()  {
-    Payment *p; 
-    
-    UPI upi(500);
-    CreditCard card(1000);
-
-    p = &upi ; 
-    p -> pay() ; 
-
-    p = &card ;
-    p -> pay() ; 
-
+    Payment *arr[2] ; 
+    arr[0] = new CreditCard(200) ; 
+    arr[1] = new UPI(100) ; 
+    for(int i = 0 ; i < 2 ; i++) {
+          arr[i] -> pay() ; 
+    }
     return 0 ; 
 }
